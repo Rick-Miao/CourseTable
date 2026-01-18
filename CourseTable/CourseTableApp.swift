40c29bc2d879875043e822294071cffb76899104
Xiaobei <1963239048@qq.com>
2026-01-18 20:53:45 +0800

    Initial Commit

---
+++ CourseTable/CourseTableApp.swift
@@ -0,0 +1,17 @@
+//
+//  CourseTableApp.swift
+//  CourseTable
+//
+//  Created by Xiaobei on 2026/1/18.
+//
+
+import SwiftUI
+
+@main
+struct CourseTableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
